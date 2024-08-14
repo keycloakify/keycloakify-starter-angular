94a16f7507dc596c10bc349ce438c20fdaf1961e
Katharina Eiserfey <42547712+kathari00@users.noreply.github.com>
2024-08-14 13:30:31 +0000

    login page

--- src/keycloak-theme/login/login/login.component.ts
+++ src/keycloak-theme/login/login/login.component.ts
@@ -5,14 +5,14 @@ import { GenericI18n_noJsx } from 'keycloakify/login/i18n/i18n';
 import { SharedService } from '../service/shared.service';
 import { Observable } from 'rxjs';
 import { SanitizeHtmlPipe } from "../../../pipes/sanitize-html.pipe";
+import { KcContext } from '../KcContext';
 
 @Component({
     selector: 'kc-login',
     templateUrl: './login.component.html',
     styleUrls: ['./login.component.scss'],
     standalone: true,
-    imports: [KcClassPipe, CommonModule, SanitizeHtmlPipe],
-    providers: [],
+    imports: [KcClassPipe, CommonModule, SanitizeHtmlPipe]
 })
 export class LoginComponent implements OnInit  {
   i18n$?: Observable<GenericI18n_noJsx<any>>;
@@ -20,17 +20,15 @@ export class LoginComponent implements OnInit  {
   
   @ViewChild('headerNode') headerNode?: TemplateRef<any>;
   @ViewChild('infoNode') infoNode?: TemplateRef<any>;
-  
   @ViewChild('socialProvidersNode') socialProvidersNode?: TemplateRef<any>;
+  @ViewChild('displayInfo') displayInfo?: boolean;
 
-  kcContext: any;
-  constructor(private sharedService: SharedService){
-    this.kcContext = window.kcContext;
- 
-  }
+  kcContext: any = window.kcContext;
+  constructor(private sharedService: SharedService){}
 
   ngOnInit(){
     this.i18n$ = this.sharedService.getI18n();
+    this.displayInfo = this.kcContext.realm.password && this.kcContext.realm?.registrationAllowed && !this.kcContext.registrationDisabled;
   }
 
 }
--- src/keycloak-theme/login/template.component.ts
+++ src/keycloak-theme/login/template.component.ts
@@ -9,6 +9,7 @@ import { SanitizeHtmlPipe } from "../../pipes/sanitize-html.pipe";
 import { createGetI18n, GenericI18n_noJsx } from 'keycloakify/login/i18n/i18n';
 import { SharedService } from './service/shared.service';
 import { Observable } from 'rxjs';
+import { DynamicStyleLoaderService } from './service/dynamicStyleLoader.service';
 export const{ getI18n } = createGetI18n({})
 @Component({
   selector: 'kc-login-template',
@@ -23,9 +24,12 @@ export class TemplateComponent implements OnInit {
   kcContext: KcContext = window.kcContext;
   displayRequiredFields = false;
   
+  displayInfo?: boolean = false;
   headerNode?: TemplateRef<any>;
   infoNode?: TemplateRef<any>
   socialProvidersNode?: TemplateRef<any>
+
+  stylesheetsLoaded = false;
   locale?: {
     supported: {
         url: string;
@@ -35,47 +39,65 @@ export class TemplateComponent implements OnInit {
     currentLanguageTag: string;
   };
 
-  i18n$: Observable<GenericI18n_noJsx<string>>
+  i18n$?: Observable<GenericI18n_noJsx<string>>
 
-  constructor(private router: Router, private cdref: ChangeDetectorRef, private sharedService: SharedService, private kcClassPipe: KcClassPipe) {
-
-    this.i18n$ = this.sharedService.getI18n();
-  }
+  constructor(private cdref: ChangeDetectorRef, private sharedService: SharedService, private kcClassPipe: KcClassPipe, private dynamicStyleLoader: DynamicStyleLoaderService, private router: Router) {}
 
   ngOnInit() {
-    
-    this.locale = this.kcContext.locale;
-      this.router.navigate(['login', { outlets: { login: this.trimPageIdSuffix(this.kcContext.pageId) }}], { skipLocationChange: true });
-
-      setTimeout(() => {
-        this.sharedService.updateI18n();
-      }, 300)
-
-      const kcBodyClass = this.kcClassPipe.transform("kcBodyClass")
-      const kcHtmlClass = this.kcClassPipe.transform("kcHtmlClass")
-      const kcHtmlClasses = kcHtmlClass.split(" ");
-
-      document.body.classList.add(kcBodyClass)
-      document.documentElement.classList.add(... kcHtmlClasses);
-
-    }
-
-    trimPageIdSuffix(pageId: string): string {
-      if (pageId.length > 4) {
-        return pageId.substring(0, pageId.length - 4);
-      }
-      return pageId;
-    }
-    
-    get publicUrl() {
-      return PUBLIC_URL;
+    this.setNavigationToPageId();
+    this.loadStyles();
+    this.applyKcIndexClasses();
+    this.i18n$ = this.sharedService.getI18n();
+    setTimeout(() => {
+      this.sharedService.updateI18n();
+    }, 300);
   }
 
-  public onRouterOutletActivate(event : any) {
-      this.cdref.detectChanges();
-      this.headerNode = event.headerNode;
-      this.infoNode = event.infoNode;
-      this.socialProvidersNode = event.socialProvidersNode;
-      
+  onRouterOutletActivate(event : any) {
+    this.cdref.detectChanges();
+    //here you can set Nodes from children like headerNode
+    this.headerNode = event.headerNode;
+    this.infoNode = event.infoNode;
+    this.socialProvidersNode = event.socialProvidersNode;      
+    this.displayInfo = event.displayInfo;
   }  
+
+  public setNavigationToPageId() {
+    this.router.navigate(['login', { outlets: { login: this.trimPageIdSuffix(window.kcContext.pageId) }}], { skipLocationChange: true });
+}
+
+private trimPageIdSuffix(pageId: string): string {
+    if (pageId.length > 4) {
+        return pageId.substring(0, pageId.length - 4);
+    }
+    return pageId;
+}
+
+  applyKcIndexClasses() {
+    const kcBodyClass = this.kcClassPipe.transform("kcBodyClass");
+    const kcHtmlClass = this.kcClassPipe.transform("kcHtmlClass");
+    
+    const kcBodyClasses = kcBodyClass.split(" ");
+    const kcHtmlClasses = kcHtmlClass.split(" ");
+
+    document.body.classList.add(...kcBodyClasses);
+    document.documentElement.classList.add(...kcHtmlClasses);
+  }
+
+  loadStyles() {
+    this.dynamicStyleLoader.loadStyle(`${this.kcContext.url.resourcesCommonPath}/node_modules/@patternfly/patternfly/patternfly.min.css`);
+    this.dynamicStyleLoader.loadStyle(`${this.kcContext.url.resourcesCommonPath}/node_modules/patternfly/dist/css/patternfly.min.css`);
+    this.dynamicStyleLoader.loadStyle(`${this.kcContext.url.resourcesCommonPath}/node_modules/patternfly/dist/css/patternfly-additions.min.css`);
+    this.dynamicStyleLoader.loadStyle(`${this.kcContext.url.resourcesCommonPath}/lib/pficon/pficon.css`)
+    this.dynamicStyleLoader.loadStyle(`${this.kcContext.url.resourcesPath}/css/login.css`);
+    this.stylesheetsLoaded = true;
+  }
+
+  tryAnotherWay() { 
+    document.forms["kc-select-try-another-way-form" as never].submit();
+  }
+
+  get publicUrl() {
+    return PUBLIC_URL;
+  }
 }
