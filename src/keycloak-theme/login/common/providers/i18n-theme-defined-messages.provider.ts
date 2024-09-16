c44a01ec0c2731db1437f66b706effdce2015682
Katharina Eiserfey <42547712+kathari00@users.noreply.github.com>
2024-09-16 19:02:47 +0000

    i18n from multiple json

--- src/keycloak-theme/login/common/providers/i18n-theme-defined-messages.provider.ts
+++ src/keycloak-theme/login/common/providers/i18n-theme-defined-messages.provider.ts
@@ -1,23 +1,46 @@
 import {
+    APP_INITIALIZER,
     EnvironmentProviders,
-    InjectionToken,
+    isDevMode,
     makeEnvironmentProviders
 } from "@angular/core";
+import { KcContextLike } from "keycloakify/login/i18n";
+import { I18nService } from "../services/i18n.service";
+import { KC_CONTEXT } from "./keycloak-context.provider";
 
-const themeDefinedMessages: Record<string, object> = {
-    en: {
-        hello: "Hello"
-    },
-    de: {
-        hello: "Hallo"
+const fetchTranslations = async (
+    kcContext: KcContextLike
+): Promise<Record<string, Record<string, string>>> => {
+    if (!isDevMode()) {
+        const translations: Record<string, Record<string, string>> = {};
+        const i18nFiles: { languageTag: string; url: string; label: string }[] =
+            kcContext?.locale?.supported || [];
+
+        for (const lang of i18nFiles) {
+            const module = await import(`../../../assets/i18n/${lang.languageTag}.json`);
+            translations[lang.languageTag] = module.default;
+        }
+        return translations;
     }
+    return {};
 };
 
-export const I18N_THEME_DEFINED_MESSAGES = new InjectionToken<Record<string, object>>(
-    "i18n theme defined messages"
-);
+export const initializeThemeDefinedMessages = (
+    i18nService: I18nService,
+    kcContext: KcContextLike
+) => {
+    return async () => {
+        const translations = await fetchTranslations(kcContext);
+        i18nService.intializeThemeDefinedMessages(translations);
+    };
+};
 
 export const provideI18nThemeDefinedMessages = (): EnvironmentProviders =>
     makeEnvironmentProviders([
-        { provide: I18N_THEME_DEFINED_MESSAGES, useValue: themeDefinedMessages }
+        {
+            provide: APP_INITIALIZER,
+            useFactory: initializeThemeDefinedMessages,
+            multi: true,
+            deps: [I18nService, KC_CONTEXT]
+        }
     ]);
