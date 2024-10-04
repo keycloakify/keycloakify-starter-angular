ea899c1f5dca1a4f068cf517c631e877ca9cf58d
Katharina Eiserfey <42547712+kathari00@users.noreply.github.com>
2024-10-04 20:02:24 +0200

    rollback extra webpack

---
+++ extra-webpack.config.js
@@ -0,0 +1,15 @@
+import path from 'node:path';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+
+export default {
+  entry: './src/main.ts',
+  output: {
+    path: path.resolve(import.meta.dirname, 'build/static/js/'),
+    publicPath: 'auto',
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '../css/[name].[contenthash].css',
+    }),
+  ],
+};
