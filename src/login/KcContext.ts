78d6fb02b0bfcc4700fa877b24aef14329b571e0
Joseph Garrone <joseph.garrone.gj@gmail.com>
2024-11-17 23:54:18 +0100

    Add link to doc comment

--- src/login/KcContext.ts
+++ src/login/KcContext.ts
@@ -2,10 +2,13 @@ import type { ExtendKcContext } from 'keycloakify/login';
 import type { KcEnvName, ThemeName } from '../kc.gen';
 
 export type KcContextExtension = {
-  themeName: ThemeName;
-  properties: Record<KcEnvName, string> & {};
+    themeName: ThemeName;
+    properties: Record<KcEnvName, string> & {};
 };
 
-export type KcContextExtensionPerPage = Record<string, Record<string, unknown>>;
+export type KcContextExtensionPerPage = {
+    // Here you can declare additional properties on the KcContext
+    // See: https://docs.keycloakify.dev/faq-and-help/some-values-you-need-are-missing-from-in-kccontext
+};
 
 export type KcContext = ExtendKcContext<KcContextExtension, KcContextExtensionPerPage>;
