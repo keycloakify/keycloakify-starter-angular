df984d9ec08b1e0fbe234a76e8e19d02641ae74a
kathari00 <katharina.eiserfey1@gmail.com>
2025-07-26 16:00:32 +0200

    Add base ref factory

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,8 +1,17 @@
 import {
     provideZonelessChangeDetection,
-    type ApplicationConfig
+    type ApplicationConfig, isDevMode
 } from '@angular/core';
+import { WELL_KNOWN_DIRECTORY_BASE_NAME } from 'keycloakify/bin/shared/constants';
+import { KC_LOGIN_CONTEXT } from '@keycloakify/angular/login/tokens/kc-context';
+import { APP_BASE_HREF } from '@angular/common';
+
+function appBaseHrefFactory(kcContext: {'x-keycloakify': {resourcesPath: string}}): string | undefined {
+    if(kcContext === undefined || isDevMode() || kcContext['x-keycloakify'].resourcesPath === undefined) return;
+    return `${kcContext['x-keycloakify'].resourcesPath}/${WELL_KNOWN_DIRECTORY_BASE_NAME.DIST}/`
+}
 
 export const appConfig: ApplicationConfig = {
-    providers: [provideZonelessChangeDetection()]
+    providers: [provideZonelessChangeDetection(),
+    {provide: APP_BASE_HREF, useFactory: appBaseHrefFactory, deps: [KC_LOGIN_CONTEXT]},],
 };
