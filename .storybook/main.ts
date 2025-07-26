8da3d0746a3f5e1bfbb8dfb84ec360388bf4e17c
kathari00 <katharina.eiserfey1@gmail.com>
2025-07-26 15:35:51 +0200

    Fix storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,10 +2,10 @@ import type { StorybookConfig } from '@storybook/angular';
 
 const config: StorybookConfig = {
     stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-    staticDirs: ['../public'],
+    addons: [],
     framework: {
-        name: '@storybook/angular',
-        options: {}
-    }
+        name: '@analogjs/storybook-angular',
+        options: {},
+    },
 };
 export default config;
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,5 +1,4 @@
 import type { Preview } from '@storybook/angular';
-import 'zone.js';
 const preview: Preview = {
     parameters: {
         controls: {
